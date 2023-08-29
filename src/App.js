09618096ea5960d331a8ca2160ec5299b60bb858
miskamero <miska.mero@gmail.com>
2023-08-29 10:37:08 +0300

    Moodle - Muistiinpanosovellus
    Probably not very efficient code...

--- src/App.js
+++ src/App.js
@@ -1,80 +1,112 @@
 import React, { useState } from 'react';
 import './App.css';
 
-const AddTask = () => {
-  const [tasks, setTasks] = useState([]);
+const AddTask = ({ addTask }) => {
+  const [newTaskHeader, setNewTaskHeader] = useState('');
   const [newTask, setNewTask] = useState('');
-  const addTask = (e) => {
+
+  const handleSubmit = (e) => {
     e.preventDefault();
-    setTasks([...tasks, newTask]);
+    addTask(newTaskHeader, newTask);
+    setNewTaskHeader('');
     setNewTask('');
   };
+
   return (
     <div>
-      <form onSubmit={addTask}>
+      <form onSubmit={handleSubmit}>
+        <label>Task Title</label><br/>
+        <input value={newTaskHeader} onChange={(e) => setNewTaskHeader(e.target.value)} /><br/>
+        <label>Task Description</label><br/>
         <input value={newTask} onChange={(e) => setNewTask(e.target.value)} />
-        <button type="submit">Lisää</button>
+        <button type="submit">Add</button>
       </form>
-      <TaskList tasks={tasks} setTasks={setTasks} />
     </div>
   );
 };
 
-const TaskList = ({ tasks, setTasks }) => {
-  const deleteTask = (task) => {
-    if (window.confirm(`Delete task ${task}?`) === false) {
-      return;
-    }else{
-      const updatedTasks = tasks.filter((t) => t !== task);
-      setTasks(updatedTasks);
+const TaskList = ({ tasks, deleteTask, editTask }) => {
+  return (
+    <div>
+      <ul>
+        {tasks.map((task, index) => (
+          <Task key={index} task={task} deleteTask={() => deleteTask(index)} editTask={(newHeader, newDescription) => editTask(index, newHeader, newDescription)} />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+const Task = ({ task, deleteTask, editTask }) => {
+  const handleEditClick = () => {
+    const newHeader = prompt('Enter a new header:', task.header);
+    const newDescription = prompt('Enter a new description:', task.description);
+
+    if (newHeader !== null && newDescription !== null) {
+      editTask(newHeader, newDescription);
     }
   };
-  return (
-    <div>
-      <ol>
-        {tasks.map((task, index) => (
-          <Task key={index} task={task} deleteTask={deleteTask} />
-        ))}
-      </ol>
-    </div>
-  );
-};
 
-const Task = ({ task, deleteTask }) => {
   return (
     <div>
       <li>
-        {task} <button type="button" onClick={() => deleteTask(task)}>Delete</button>
+        {task.header && <h3>{task.header}</h3>}
+        {task.description}
+        <button type="button" onClick={handleEditClick}>Edit</button>
+        <button type="button" onClick={deleteTask}>Delete</button>
       </li>
     </div>
   );
 };
 
 const App = () => {
-  // Function that disables confirmation when deleting a task
-  let confirm = 0;
-  const confirmationDisable = () => {
-    if(confirm === 0){
-      confirm = 1;
-      document.querySelector('.confirmButton').innerHTML = 'Toggle delete confirmation (disabled)';
-      document.querySelector('.confirmButton').style.backgroundColor = 'red';
-      document.querySelector('.confirmButton').style.borderColor = 'red';
-    }else{
-      confirm = 0;
-      document.querySelector('.confirmButton').innerHTML = 'Toggle delete confirmation (enabled)';
-      document.querySelector('.confirmButton').style.backgroundColor = 'green';
-      document.querySelector('.confirmButton').style.borderColor = 'green';
+  const [tasks, setTasks] = useState([]);
+  const [isConfirmationEnabled, setIsConfirmationEnabled] = useState(true);
+
+  const addTask = (header, description) => {
+    const newTask = { header, description };
+    setTasks([...tasks, newTask]);
+  };
+
+  const handleDeleteTask = (index) => {
+    if (!isConfirmationEnabled || window.confirm('Are you sure you want to delete this task?')) {
+      const updatedTasks = tasks.filter((_, i) => i !== index);
+      setTasks(updatedTasks);
     }
   };
+
+  const toggleConfirmation = () => {
+    setIsConfirmationEnabled(!isConfirmationEnabled);
+  };
+
+  const clearTaskList = () => {
+    if (!isConfirmationEnabled || window.confirm('Are you sure you want to clear the task list?')) {
+      setTasks([]);
+    } else {
+      return;
+    }
+  };
+
+  const confirmationButtonClass = isConfirmationEnabled ? 'confirmButtonEnabled' : 'confirmButtonDisabled';
+
+  const editTask = (index, newHeader, newDescription) => {
+    const updatedTasks = tasks.map((task, i) =>
+      i === index ? { ...task, header: newHeader, description: newDescription } : task
+    );
+    setTasks(updatedTasks);
+  };
+
   return (
     <div className='mainDiv'>
       <h1>Task List</h1>
-      {/* Button that disables confirmation when deleting a task */}
-      <button className='confirmButton' type="button" onClick={() => confirmationDisable()}>Toggle delete confirmation (Enabled)</button>
-      {/* Button that clears the task list */}
-      <button className='clearButton' type="button" onClick={() => window.location.reload()}>Clear task list</button>
+      <button className={`confirmButton ${confirmationButtonClass}`} type="button" onClick={toggleConfirmation}>
+        Toggle delete confirmation ({isConfirmationEnabled ? 'Enabled' : 'Disabled'})
+      </button>
+      <br/>
+      <button className='clearButton' type="button" onClick={clearTaskList}>Clear task list</button>
       <br />
-      <AddTask />
+      <AddTask addTask={addTask} />
+      <TaskList tasks={tasks} deleteTask={handleDeleteTask} editTask={editTask} />
     </div>
   );
 };
